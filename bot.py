d0e0a19a7c5ac1f496ea96823e12d07f29df5e19
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2021-12-14 18:50:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,8 +69,9 @@ async def _help(bot, update):
 
 
 # Store file
-@xbot.on_message(filters.media & filters.private)
+@xbot.on_message(filters.command('store') & filters.private)
 async def _main(bot, update):
+    await update.reply_text("Processing", True)
     if OWNER_ID == 'all':
         pass
     elif int(OWNER_ID) == update.from_user.id:
